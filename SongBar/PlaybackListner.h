1b4b09d322dd6c49d03df0c39cf02d06383c0a8b
Justin Oakes <justin.t.oakes@gmail.com>
2020-10-12 14:52:53 -0500

    added entitlement to Info.plist to make Scripting bridge work

--- SongBar/PlaybackListner.h
+++ SongBar/PlaybackListner.h
@@ -6,16 +6,19 @@
 //  Copyright © 2020 corpe. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-#import <ScriptingBridge/ScriptingBridge.h>
+#import "Cocoa/Cocoa.h"
+#import "ScriptingBridge/ScriptingBridge.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PlaybackListner : NSObject
 
 @property (strong, nonatomic) NSString *menuTitle;
+@property( class, copy ) NSString* musicBundleIdentifier;
+@property( class, copy ) NSString* spotifyBundleIdentifier;
 
 - (void) refresh;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- SongBar/PlaybackListner.m
+++ SongBar/PlaybackListner.m
@@ -23,32 +23,69 @@
 {
     self = [super init];
     if (self) {
-        _musicApplication = [SBApplication applicationWithBundleIdentifier:@"com.apple.Music"];
-        _spotifyApplication = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
-        [self updateMenuTitle];
+        _musicApplication = [SBApplication applicationWithBundleIdentifier:[PlaybackListner musicBundleIdentifier]];
+        _spotifyApplication = [SBApplication applicationWithBundleIdentifier:[PlaybackListner spotifyBundleIdentifier]];
+        [self refresh];
         [self configureObservers];
     }
     return self;
 }
 
+// Static Bundle Identifiers
++ (NSString*) musicBundleIdentifier
+{
+    return @"com.apple.Music";
+}
+
++ (void) setMusicBundleIdentifier:(NSString*)newStr {}
+
++ (NSString*) spotifyBundleIdentifier
+{
+    return @"com.spotify.client";
+}
+
++ (void) setSpotifyBundleIdentifier:(NSString*)newStr {}
+    
 - (void) configureObservers {
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self
-                                                        selector:@selector(updateMenuTitle)
+                                                        selector:@selector(refresh)
                                                             name:@"com.apple.iTunes.playerInfo"
                                                           object:nil];
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self
-                                                        selector:@selector(updateMenuTitle)
+                                                        selector:@selector(refresh)
                                                             name:@"com.spotify.client.PlaybackStateChanged"
                                                           object:nil];
 }
 
 - (void)refresh {
-    [self updateMenuTitle];
+    BOOL iTunesOpen = [self applicationOpenWithBundleId:[PlaybackListner musicBundleIdentifier]];
+    BOOL spotifyOpen = [self applicationOpenWithBundleId:[PlaybackListner spotifyBundleIdentifier]];
+    NSString *iTunesString;
+    NSString *spotifyString;
+    if (iTunesOpen && self.musicApplication.playerState == MusicEPlSPlaying) {
+        iTunesString = [self getTrackFrom:_musicApplication];
+    }
+    if (spotifyOpen && self.spotifyApplication.playerState == SpotifyEPlSPlaying) {
+        spotifyString = [self getTrackFrom:_spotifyApplication];
+    }
+
+    [self updateMenuTitleWithString:@"SongBar"];
 }
 
-- (void) updateMenuTitle {
-    [self setValue:@"SongBar" forKey:@"menuTitle"];
-    NSLog(@"I ran %@", self.menuTitle);
+- (void) updateMenuTitleWithString:(NSString *)title {
+    [self setValue:title forKey:@"menuTitle"];
 }
 
+- (BOOL)applicationOpenWithBundleId:(NSString *)bundleId {
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"bundleIdentifier == %@", bundleId];
+    NSArray<NSRunningApplication *> *runningAplications = [[[NSWorkspace sharedWorkspace] runningApplications] filteredArrayUsingPredicate:predicate];
+    
+    return ([runningAplications count] > 0);
+}
+
+- (NSString *)getTrackFrom:(SBApplication *)application {
+    MusicTrack *currentTrack = [application performSelector:@selector(currentTrack)];
+    NSLog(@"%@", currentTrack);
+    return @"SongBar";
+}
 @end
