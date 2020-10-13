b0334558421afb215db5d5413290205f8c925636
Justin Oakes <justin.t.oakes@gmail.com>
2020-10-13 16:30:02 -0500

    added view to menu item

--- SongBar/PlaybackView.swift
+++ SongBar/PlaybackView.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2020 corpe. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+class PlaybackView: NSView {
+    
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var pausePlayButton: NSButton!
+    @IBOutlet weak var nextButton: NSButton!
+    @IBOutlet weak var backButton: NSButton!
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        loadFromNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadFromNib()
+    }
+    
+    private func loadFromNib() {
+        var nibObjects: NSArray?
+        Bundle.main.loadNibNamed(NSNib.Name(stringLiteral: "PlaybackView"),
+                                 owner: self,
+                                 topLevelObjects: &nibObjects)
+        let view = nibObjects?.filter({ $0 is NSView }).first as! NSView
+        addSubview(view)
+    }
+    
+}
