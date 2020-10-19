8e176f56134dea221abd6c8f07740f0197a44b59
Justin Oakes <justin.t.oakes@gmail.com>
2020-10-19 10:14:18 -0500

    truncates long title

--- SongBar/AppDelegate.swift
+++ SongBar/AppDelegate.swift
@@ -27,9 +27,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         sysBar.menu = menu
         menuTitleObserver = playbackListner.observe(\PlaybackListner.menuTitle,
                                 options: .new) { (listner, title) in
-            self.sysBar.title = title.newValue
+            self.sysBar.title = self.menuTitleOfMaximumLength(title: title.newValue)
         }
         playbackListner.populateMusicData()
+    }
+    
+    private func menuTitleOfMaximumLength(title: String?) -> String {
+        let maximumLength = 57
+        let elipsiesLength = 3
+        guard var title = title else { return "SongBar" }
+        if title.count > maximumLength {
+            let lengthToTrim = ((maximumLength + elipsiesLength) - title.count) * -1
+            let startIndex = title.index(title.startIndex, offsetBy: (title.count / 2) - (lengthToTrim / 2))
+            let endIndex = title.index(title.startIndex, offsetBy: (title.count / 2) + (lengthToTrim / 2))
+            
+            title.replaceSubrange(startIndex...endIndex, with: "...")
+        }
+        return title
     }
 
 }
