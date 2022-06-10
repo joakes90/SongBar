08dfd376cfbf618732730fed81fd51859ffb515d
Justin Oakes <justin.t.oakes@gmail.com>
2022-06-10 09:16:18 -0500

    updated package collections to modern spm API

--- SongBar/PlaybackMenuItem.swift
+++ SongBar/PlaybackMenuItem.swift
@@ -14,6 +14,7 @@ class PlaybackMenuItem: NSMenuItem {
         view as? PlaybackView
     }
 
+    @MainActor
     required init(coder: NSCoder) {
         super.init(coder: coder)
         let playbackView = PlaybackView(frame: NSRect(x: 0.0,
